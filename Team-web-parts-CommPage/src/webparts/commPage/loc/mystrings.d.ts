37616c9099a8f6dee531f72c69361ebcb1b727c7
MeryemNairi <meriemnairi01@gmail.com>
2024-05-15 14:07:49 +0100

    first commit

---
+++ Team-web-parts-CommPage/src/webparts/commPage/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ICommPageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'CommPageWebPartStrings' {
+  const strings: ICommPageWebPartStrings;
+  export = strings;
+}
